c83263b469ae212699a1a44b70f4c131a8ee29e8
Manoj N <manoj99eng@gmail.com>
2026-09-11 16:22:23 +0530

    Update backend.tf

--- 1-network/backend.tf
+++ 1-network/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "itkannadigaru-infra-statefile-backup"
-    key            = "itkannadigaru/1-network/terraform.tfstate"
+    bucket         = "manoj99eng-infra-statefile-backup"
+    key            = "manoj99eng/1-network/terraform.tfstate"
     region         = "us-west-2"
-    dynamodb_table = "itkannadigaru-terraform-locks"
+    dynamodb_table = "manoj99eng-terraform-locks"
     encrypt        = true
   }
 }
