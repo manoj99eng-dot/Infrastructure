737bee6def428fef05c508f9d2a81bef6c7430c5
Manoj N <manoj99eng@gmail.com>
2026-09-11 16:26:09 +0530

    Update variables.tf

--- 2-eks/variables.tf
+++ 2-eks/variables.tf
@@ -1,15 +1,15 @@
 variable "project" {
   type    = string
-  default = "itkannadigaru"
+  default = "manoj99eng"
 }
 
 variable "kubernetes_version" {
   type    = string
-  default = "1.30"
+  default = "1.34"
 }
 
 variable "ssh_key_name" {
   type        = string
   description = "Name of the EC2 key pair for SSH access to worker nodes"
-  default     = "itkannadigaru"
+  default     = "manoj-agni"
 }
