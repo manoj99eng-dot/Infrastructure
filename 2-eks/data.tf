f89c83240858e41e3faacc533ff3e01956ae53c6
Manoj N <manoj99eng@gmail.com>
2026-09-11 16:25:00 +0530

    Update data.tf

--- 2-eks/data.tf
+++ 2-eks/data.tf
@@ -2,8 +2,8 @@ data "terraform_remote_state" "network" {
   backend = "s3"
 
   config = {
-    bucket = "itkannadigaru-infra-statefile-backup"
-    key    = "itkannadigaru/1-network/terraform.tfstate"
+    bucket = "manoj99eng-infra-statefile-backup"
+    key    = "manoj99eng/1-network/terraform.tfstate"
     region = "us-west-2"
   }
 }
